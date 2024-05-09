1aa75002a29381b74dd0c04dc675dbb2b0166bf1
MaxKrasnoloskyi <makskrasnopolskyi@gmail.com>
2024-05-09 13:37:58 +0300

    changed field and source link

--- main.go
+++ main.go
@@ -1,14 +1,10 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
-	"github.com/go-chi/chi"
-	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 	"log"
-	"net/http"
 	"os"
 	"scratch/internal/database"
 )
@@ -20,7 +16,7 @@ type apiConfig struct {
 func main() {
 	fmt.Println("START")
 
-	feed, err := urlToFeed("https://djinni.co/jobs/?primary_keyword=Scala&exp_level=5y")
+	feed, err := urlToFeed("https://djinni.co/jobs/rss/?primary_keyword=Java&exp_level=1y")
 	if err != nil {
 		log.Println("Error caused feeding data", err)
 	}
@@ -43,41 +39,41 @@ func main() {
 	fmt.Printf("PORT: %v", portString)
 	fmt.Printf("DB_URL: %v", dbUrl)
 
-	conn, err := sql.Open("postgres", dbUrl)
-	if err != nil {
-		log.Fatal("Can't open connection with DATABASE", err)
-	}
+	//conn, err := sql.Open("postgres", dbUrl)
+	//if err != nil {
+	//	log.Fatal("Can't open connection with DATABASE", err)
+	//}
 
-	apiConfig := apiConfig{
-		DB: database.New(conn),
-	}
+	//apiConfig := apiConfig{
+	//	DB: database.New(conn),
+	//}
 
-	// Create a new router
-	router := chi.NewRouter()
-
-	router.Use(cors.Handler(cors.Options{
-		AllowedOrigins:   []string{"https://*", "http://*"},
-		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowedHeaders:   []string{"*"},
-		ExposedHeaders:   []string{"Link"},
-		AllowCredentials: false,
-		MaxAge:           300,
-	}))
-
-	v1Router := chi.NewRouter()
-	v1Router.Get("/users", handleGet)
-	v1Router.Get("/err", handleError)
-	v1Router.Post("/users", apiConfig.handlerCreateUser)
-
-	router.Mount("/v1", v1Router)
-
-	srv := &http.Server{
-		Handler: router,
-		Addr:    ":" + portString,
-	}
-	fmt.Println("Server is starting ...")
-	servErr := srv.ListenAndServe()
-	if servErr != nil {
-		log.Fatal("Server problem")
-	}
+	//// Create a new router
+	//router := chi.NewRouter()
+	//
+	//router.Use(cors.Handler(cors.Options{
+	//	AllowedOrigins:   []string{"https://*", "http://*"},
+	//	AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+	//	AllowedHeaders:   []string{"*"},
+	//	ExposedHeaders:   []string{"Link"},
+	//	AllowCredentials: false,
+	//	MaxAge:           300,
+	//}))
+	//
+	//v1Router := chi.NewRouter()
+	//v1Router.Get("/users", handleGet)
+	//v1Router.Get("/err", handleError)
+	//v1Router.Post("/users", apiConfig.handlerCreateUser)
+	//
+	//router.Mount("/v1", v1Router)
+	//
+	//srv := &http.Server{
+	//	Handler: router,
+	//	Addr:    ":" + portString,
+	//}
+	//fmt.Println("Server is starting ...")
+	//servErr := srv.ListenAndServe()
+	//if servErr != nil {
+	//	log.Fatal("Server problem")
+	//}
 }
--- rss.go
+++ rss.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	"encoding/xml"
 	"io"
 	"log"
@@ -11,26 +10,22 @@ import (
 
 type RSSFeed struct {
 	Channel struct {
-		Title         string        `xml:"title"`
-		Link          string        `xml:"link"`
-		Description   string        `xml:"description"`
-		Category      []RSSCategory `xml:"category"`
-		LastBuildDate string        `xml:"lastBuildDate"`
-		Item          []RSSItem     `xml:"item"`
+		Title         string    `xml:"title"`
+		Link          string    `xml:"link"`
+		Description   string    `xml:"description"`
+		Category      []string  `xml:"category"`
+		LastBuildDate string    `xml:"lastBuildDate"`
+		Item          []RSSItem `xml:"item"`
 	} `xml:"channel"`
 }
 
-type RSSCategory struct {
-	Category string `xml:"category"`
-}
-
 type RSSItem struct {
-	Title       string      `xml:"title"`
-	Link        string      `xml:"link"`
-	Description string      `xml:"description"`
-	PubDate     string      `xml:"pubDate"`
-	Guid        string      `xml:"guid"`
-	Category    RSSCategory `xml:"category"`
+	Title       string   `xml:"title"`
+	Link        string   `xml:"link"`
+	Description string   `xml:"description"`
+	PubDate     string   `xml:"pubDate"`
+	Guid        string   `xml:"guid"`
+	Category    []string `xml:"category"`
 }
 
 func urlToFeed(url string) (RSSFeed, error) {
@@ -46,19 +41,16 @@ func urlToFeed(url string) (RSSFeed, error) {
 
 	defer resp.Body.Close()
 
-	// Preprocess XML data to replace &nbsp; with a space
-	var buf bytes.Buffer
-	if _, err := io.Copy(&buf, resp.Body); err != nil {
-		log.Println("Reading problem")
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Println("Read problem")
 		return RSSFeed{}, err
 	}
-	xmlData := buf.Bytes()
-	xmlData = bytes.Replace(xmlData, []byte("&nbsp;"), []byte(" "), -1)
+
 	rssFeed := RSSFeed{}
 
 	// Unmarshal preprocessed XML data
-	rssFeed = RSSFeed{}
-	if err := xml.Unmarshal(xmlData, &rssFeed); err != nil {
+	if err := xml.Unmarshal(data, &rssFeed); err != nil {
 		log.Println("Unmarshal problem")
 		return RSSFeed{}, err
 	}
